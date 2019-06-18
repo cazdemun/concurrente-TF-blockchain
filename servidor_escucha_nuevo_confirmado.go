31623e65a52f8517be156a60c475b3ae1b3adc28
merRen22 <renatomercadoluna@hotmail.com>
2019-06-18 12:40:35 -0500

    clase servidor escucha nuevo

---
+++ servidor_escucha_nuevo_confirmado.go
@@ -0,0 +1,21 @@
+var ips = make(string[])
+ startServer() {
+	fmt.Println("Iniciando Servidor guardador...")
+	ln, _:= net.Listen("tcp", "10.11.97.218:8123")
+	defer ln.Close()
+	fmt.Println("Escuchando por puerto 8123")
+	for {
+		con, _ := ln.Accept()
+		go handleServerRequest(con)
+	}
+}
+
+func handleServerRequest(con net.Conn) {
+	defer con.Close()
+	r := bufio.NewReader(con)
+	for {
+		msg, _ := r.ReadString('\n')
+		fmt.Print("IP nueva recibida: ",msg)
+		ips = append(ips,msg)
+	}
+}

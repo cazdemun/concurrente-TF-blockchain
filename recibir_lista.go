53ab5ed8712baea2461f6ead8f0f60c41122de7d
xeroot <r10500@hotmail.com>
2019-06-18 12:26:35 -0500

    connect_to_network

--- recibir_lista.go
+++ recibir_lista.go
@@ -1,14 +1,30 @@
 package main
-
 import (
 	"fmt"
-	"time"
+	"net"
+	"bufio"
+	"os"
 )
 
+var ips = make(int[])
+
+func connect_to_network(ip_to_connect string) {
+	fmt.Println("Uniendose a la red...")
+	con,_ := net.Dial("tcp", ip_to_connect)
+	defer con.Close()
+	r := bufio.NewReader(con)
+	for {
+		fmt.Print("Mensaje: ")
+		msg = "xd"
+		fmt.Fprint(con,msg)
+		resp,_ := r.ReadString('\n')
+		fmt.Print("Respuesta: ",resp)
+		if msg[0] == 'x' {
+			break;
+		}
+	}
+}
+
 func main() {
-	ch := make(chan string)
-	go Productor(1,ch)
-	go Consumidor(2,ch)
-	time.Sleep(time.Second);
-	fmt.Print("Acabo.\n")
+	connect_to_network("10.11.97.218:8000")
 }

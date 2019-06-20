881c7dc3ecd89dcee38de712a32229d72d9027ef
xeroot <r10500@hotmail.com>
2019-06-19 22:07:33 -0500

    nombre de la clase cambiado

--- conectarse_a_la_red.go
+++ conectarse_a_la_red.go
@@ -1,22 +1,23 @@
 package main
+
 import (
+	"bufio"
 	"fmt"
 	"net"
-	"bufio"
 )
 
-var ips = make([]string,0)
+var ips = make([]string, 0)
 
 func connect_to_network(ip_to_connect string) {
 	fmt.Println("Uniendose a la red...")
-	con,_ := net.Dial("tcp", ip_to_connect)
+	con, _ := net.Dial("tcp", ip_to_connect)
 	defer con.Close()
 	r := bufio.NewReader(con)
 	msg := "soy nuevo = ...."
 	fmt.Println("Enviando mi ip...")
-	fmt.Fprint(con,msg)
+	fmt.Fprint(con, msg)
 	fmt.Println("Recibiendo las demas ips...")
-	resp,_ := r.ReadString('\n')
+	resp, _ := r.ReadString('\n')
 	fmt.Print(resp)
 	// agregar aqui...
 	fmt.Println("Listo.")
